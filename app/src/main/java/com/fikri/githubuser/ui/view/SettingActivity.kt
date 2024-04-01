6b8ba335f011c362f8b23fe152726fb0354122e5
MhdFikri44 <mhdfikr44@gmail.com>
2024-04-01 15:20:30 +0700

    modify and add file

---
+++ app/src/main/java/com/fikri/githubuser/ui/view/SettingActivity.kt
@@ -0,0 +1,40 @@
+package com.fikri.githubuser.ui.view
+
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
+import com.fikri.githubuser.databinding.ActivitySettingBinding
+import com.fikri.githubuser.utils.SettingPreferences
+import com.fikri.githubuser.utils.dataStore
+
+class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val switchTheme = binding.switchTheme
+
+        val pref = SettingPreferences.getInstance(application.dataStore)
+        val settingViewModel =
+            ViewModelProvider(this, ViewModelFactory(pref))[SettingViewModel::class.java]
+
+        settingViewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            settingViewModel.saveThemeSetting(isChecked)
+        }
+    }
+}
